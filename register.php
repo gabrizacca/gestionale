8173af2e248316877e29c2fe72a35996655a8d2d
ValePicciuz <valepicci2000@gmail.com>
2026-03-05 09:57:22 +0100

    Add files via upload

--- register.php
+++ register.php
@@ -20,13 +20,19 @@ try {
 }
 
 // Controllo se i dati del form di registrazione sono stati inviati
-if (!empty($_POST['fullname']) && !empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['confirm_password'])) {
+if (!empty($_POST['nome']) && !empty($_POST['cognome']) && !empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password']) && !empt y($_POST['confirm_password']) && !empty($_POST[''])) {
 
-    $fullname        = trim($_POST['fullname']);
+    $name            = trim($_POST['nome']);
+    $cogome          = trim($_POST['cognome']);
     $usernameInput   = trim($_POST['username']);
     $email           = trim($_POST['email']);
     $passwordInput   = $_POST['password'];
     $confirmPassword = $_POST['confirm_password'];
+    $data_assunzione = DateTime();
+    $stipendio       = trim($_POST['stipendio']);
+    $IBAN            = trim($_POST['IBAN']);
+    $tipo            = trim($_POST['tipo']);
+    $is_admin        = trim($_POST['is_admin']);
 
     // Validazione lato server
 
@@ -69,13 +75,17 @@ if (!empty($_POST['fullname']) && !empty($_POST['username']) && !empty($_POST['e
     $insertSql  = "INSERT INTO dipendenti (nome_completo, username, email, pswd) VALUES (:nome_completo, :username, :email, :pswd)";
     $insertStmt = $pdo->prepare($insertSql);
     $insertStmt->execute([
-        'nome_completo' => $fullname,
+        'nome'          => $name,
+        'cognome'       => $cognome,
+        'Data_Assunzione' => $now
         'username'      => $usernameInput,
         'email'         => $email,
         'pswd'          => $passwordHash,
+        'stipendio'     => $stipendio,
+
     ]);
 
-    echo "Registrazione completata con successo! Benvenuto, " . htmlspecialchars($fullname) . ".";
+    echo "Registrazione completata con successo! Benvenuto, " . htmlspecialchars($name) . ".";
     // Puoi reindirizzare al login: header('Location: login.php');
 
 } else {
