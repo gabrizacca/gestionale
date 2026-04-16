947c06d32b839cef51b5a718a5bc71dff6788bfe
ValePicciuz <valepicci2000@gmail.com>
2026-04-16 09:23:31 +0200

    Delete Dashboard.php

--- Dashboard.php
+++
@@ -1,6 +0,0 @@
-<?php
-    if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
-        header("Location: index.html"); // Reindirizza al login
-        exit; // Fondamentale per fermare l'esecuzione
-    }
-?>
